225fb07111fb4e9ea219e729e3976757189b5204
Khang NGUYEN QUOC <nguyenquockhang117@gmail.com>
2021-04-18 23:47:13 +0200

    add fs getFileList check_exists, add test folder

---
+++ include/dup_scan.hpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+bool check_exists(const fs::path &p, fs::file_status s = fs::file_status{})
+{
+    // std::cout << p;
+    if (fs::status_known(s) ? fs::exists(s) : fs::exists(p))
+    {
+        return true;
+    }
+    return false;
+}
+
+// fs::path convertToFsPath(const string iPath)
+// {
+//     fs::path a = fs::u8path( u8(iPath));
+//     return a;
+// }
+
+void getFileList(const string iScanPath, vector<string> &oFileList)
+{
+    if (iScanPath == "")
+    {
+        return;
+    }
+
+    fs::path current_path(iScanPath);
+    if (check_exists(current_path))
+    {
+        for (const auto &entry : fs::directory_iterator(current_path))
+        {
+            const auto filenameStr = entry.path().filename().string();
+            // cout << "entry: " << filenameStr << endl;
+
+            if (entry.is_directory())
+            {
+                // std::cout << "dir:  " << filenameStr << '\n';
+                getFileList(entry.path().string(), oFileList);
+            }
+            else if (entry.is_regular_file())
+            {
+                // std::cout << "file: " << filenameStr << '\n';
+
+                oFileList.push_back(entry.path().string());
+            }
+            // else
+            //     std::cout << "??    " << filenameStr << '\n';
+        }
+    }
+    else
+    {
+        cout << "Path does not exists!\n"
+             << iScanPath << "\n";
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
 #include <folder.hpp>
 #include <filesystem>
+#include <dup_scan.hpp>
 
 using namespace std;
 namespace fs = std::filesystem;
 
 int main(int argc, const char**argv)
 {
-    cout << "project init!\n";
-    Folder a;
+    // cout << "project init!\n";
+    // Folder a;
 
-    a.setSize(100);
+    // a.setSize(100);
 
-    cout << a.getSize() << endl;
-    const fs::path pathToShow{ argc >= 2 ? argv[1] : fs::current_path() };
+    // cout << a.getSize() << endl;
+    // const fs::path pathToShow{ argc >= 2 ? argv[1] : fs::current_path() };
 
-    cout << "current path: " << fs::current_path().string() << endl ;
+    // cout << "current path: " << fs::current_path().string() << endl ;
 
-    for (const auto& entry : fs::directory_iterator(pathToShow)) {
-        const auto filenameStr = entry.path().filename().string();
-        // cout << "entry: " << filenameStr << endl;
+    // for (const auto& entry : fs::directory_iterator(pathToShow)) {
+    //     const auto filenameStr = entry.path().filename().string();
+    //     // cout << "entry: " << filenameStr << endl;
 
-        if (entry.is_directory()) {
-            std::cout << "dir:  " << filenameStr << '\n';
-        }
-        else if (entry.is_regular_file()) {
-            std::cout << "file: " << filenameStr << '\n';
-        }
-        else
-            std::cout << "??    " << filenameStr << '\n';
+    //     if (entry.is_directory()) {
+    //         std::cout << "dir:  " << filenameStr << '\n';
+    //     }
+    //     else if (entry.is_regular_file()) {
+    //         std::cout << "file: " << filenameStr << '\n';
+    //     }
+    //     else
+    //         std::cout << "??    " << filenameStr << '\n';
+    // }
+
+    // cout << check_exists("yhvgt") << endl;
+    vector<string> fileList;
+    getFileList(fs::current_path().string(), fileList);
+
+    for(string file: fileList)
+    {
+        cout << "File: " << file << endl;
     }
 
 
