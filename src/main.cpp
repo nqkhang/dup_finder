53cd3ac9ee0b004c13daf102ea78d8c02fe40672
Khang NGUYEN QUOC <nguyenquockhang117@gmail.com>
2021-04-18 19:06:52 +0200

    update cmake to compile as c++ 17, ignore .vscode folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <folder.hpp>
+#include <filesystem>
 
 using namespace std;
+namespace fs = std::filesystem;
 
-int main()
+int main(int argc, const char**argv)
 {
     cout << "project init!\n";
     Folder a;
@@ -11,6 +13,24 @@ int main()
     a.setSize(100);
 
     cout << a.getSize() << endl;
+    const fs::path pathToShow{ argc >= 2 ? argv[1] : fs::current_path() };
+
+    cout << "current path: " << fs::current_path().string() << endl ;
+
+    for (const auto& entry : fs::directory_iterator(pathToShow)) {
+        const auto filenameStr = entry.path().filename().string();
+        // cout << "entry: " << filenameStr << endl;
+
+        if (entry.is_directory()) {
+            std::cout << "dir:  " << filenameStr << '\n';
+        }
+        else if (entry.is_regular_file()) {
+            std::cout << "file: " << filenameStr << '\n';
+        }
+        else
+            std::cout << "??    " << filenameStr << '\n';
+    }
+
 
     return 0;
 }
